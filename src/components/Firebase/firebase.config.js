2e59f40bf67d8e1a61efbadf40de44a5f89dac92
Hassan Ali <hassan4np@gmail.com>
2024-05-04 05:07:58 +0600

    update design

--- src/components/Firebase/firebase.config.js
+++ src/components/Firebase/firebase.config.js
@@ -4,12 +4,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyD2u5MrHDqYyyFMiqfm8VkFsm9dQxF9sIg",
-    authDomain: "b8a12-project.firebaseapp.com",
-    projectId: "b8a12-project",
-    storageBucket: "b8a12-project.appspot.com",
-    messagingSenderId: "251049278867",
-    appId: "1:251049278867:web:a14035caea524e075dfc66"
+    // apiKey: "AIzaSyD2u5MrHDqYyyFMiqfm8VkFsm9dQxF9sIg",
+    // authDomain: "b8a12-project.firebaseapp.com",
+    // projectId: "b8a12-project",
+    // storageBucket: "b8a12-project.appspot.com",
+    // messagingSenderId: "251049278867",
+    // appId: "1:251049278867:web:a14035caea524e075dfc66"
 };
 
 const app = initializeApp(firebaseConfig);
